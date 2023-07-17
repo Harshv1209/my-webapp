65485bbfd1f9c636ac2026252b2787f9dfc07055
Harshv1209 <1320204019@bitraipur.ac.in>
2023-07-18 02:23:25 +0530

    made counter with adjusting increment and decrement

---
+++ src/pages/harsh/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div><h1>JAI SHREE RAM 🚩</h1></div>
+  )
+}
+
+export default index
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
 import { Inter } from 'next/font/google'
+import {Stack,Button,Typography} from '@mui/material'
+import {useState} from 'react';
 
 export default function Home() {
+  const [counter,setCounter]=useState(0)
+  const increment=()=>{
+    setCounter(counter+1)
+  }
+  const decrement=()=>{
+    setCounter(counter-1)
+  }
   return (
-    <h1>HELLO WORLD!</h1>
-  )
-}
+    <Stack direction={'row'} spacing={2} alignItems={'center'}>
+      <Typography variant='h3'>Counter Value  : {counter}</Typography>
+      <Button variant="contained" sx={{
+        backgroundColor:'green',
+        width:'150px'
+      }}
+      onClick={increment}>Inrement</Button>
+       <Button variant="contained" sx={{
+        backgroundColor:'',
+        width:'150px'
+      }}
+      onClick={decrement}>Decrement</Button>
+      </Stack>
+  )} 
